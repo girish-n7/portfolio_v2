fcfc1918880a6e8bae6f8534a0a122419c99490c
Girish_N <ngirish1729@gmail.com>
2023-10-04 18:22:03 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SwitchPage from "./components/SwitchPage";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <SwitchPage />
+      <Footer />
+    </>
+  );
+}
