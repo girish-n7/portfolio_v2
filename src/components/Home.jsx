a4dcd753228d0330429cf38ab09d672eb91e5be6
Girish_N <ngirish1729@gmail.com>
2023-10-05 22:06:03 +0530

    style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import right from "../assets/right.svg";
+import left from "../assets/left.svg";
 
 export default function Home() {
   let navigate = useNavigate();
@@ -33,15 +35,23 @@ export default function Home() {
 
   return (
     <div className="home--container">
-      <div className="home--intro" style={{ width: start ? "70%" : "90%" }}>
-        <p>
+      <div
+        className="home--intro__container"
+        style={{
+          width: start ? "70%" : "90%",
+        }}
+      >
+        <p className="home--intro">
           Hi, my name is <span className="name">Girish N.</span>
         </p>
-        <p>
+        <p className="home--intro__sub">
           I am a Web Developer. Checkout my projects or head to the contact page
           to get in touch with me.
         </p>
-        {!start && <button onClick={handleStart}>Start</button>}
+
+        <button onClick={handleStart} className="start--button">
+          <img src={start ? left : right} className="start--img" alt=""></img>
+        </button>
       </div>
       <div
         className={`home--options ${!start && "hide"}`}
