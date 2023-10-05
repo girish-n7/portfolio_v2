cf4149ba50a8b2d716b38d7f90379d4b8f45a630
Girish_N <ngirish1729@gmail.com>
2023-10-05 19:02:59 +0530

    style update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   let navigate = useNavigate();
 
+  let [start, setStart] = useState(false);
+
   //create a options array
   let optionsArr = [
     { name: "Home", route: "/" },
     { name: "Projects", route: "/projects" },
-    { name: "Contact me", route: "/contact" },
+    { name: "Contact", route: "/contact" },
     { name: "About", route: "/about" },
   ];
 
@@ -24,9 +27,13 @@ export default function Home() {
     );
   });
 
+  function handleStart() {
+    setStart((prevState) => !prevState);
+  }
+
   return (
     <div className="home--container">
-      <div className="home--intro">
+      <div className="home--intro" style={{ width: start ? "70%" : "90%" }}>
         <p>
           Hi, my name is <span className="name">Girish N.</span>
         </p>
@@ -34,8 +41,16 @@ export default function Home() {
           I am a Web Developer. Checkout my projects or head to the contact page
           to get in touch with me.
         </p>
+        {!start && <button onClick={handleStart}>Start</button>}
       </div>
-      <div className="home--options">{optionsMap}</div>
+      <div
+        className={`home--options ${!start && "hide"}`}
+        style={{
+          width: start ? "28%" : "0%",
+        }}
+      >
+        {optionsMap}
+      </div>
     </div>
   );
 }
