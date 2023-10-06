40562e4684d23b330716b50e2268a338c730c0fa
Girish_N <ngirish1729@gmail.com>
2023-10-06 22:14:22 +0530

    style, project

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,17 +1,44 @@
 /* eslint-disable react/prop-types */
+import summarizer from "../assets/summarizer_live.png";
+import chatbot from "../assets/chatbot_live.png";
+import portfolio from "../assets/portfolio_live.png";
+import eCommerce from "../assets/e-commerce_live.png";
 
 export default function ProjectCard({ data }) {
+  let imgSource =
+    data.name === "Summarizer"
+      ? summarizer
+      : data.name === "Chatbot"
+      ? chatbot
+      : data.name === "e-commerce website"
+      ? eCommerce
+      : portfolio;
   return (
     <div className="project--card">
-      <img className="project--img" src="" alt=""></img>
+      {data.links && (
+        <img className="project--img" src={imgSource} alt=""></img>
+      )}
       <div className="project--body">
         <p className="project--title">{data.name}</p>
+        <hr className="project--hr" />
         <p className="project--tech">{data.tech}</p>
         <p className="project--desc">{data.desc}</p>
-        <div className="link--container">
-          <button className="live--link">{data.links.live}</button>
-          <button className="github--link">{data.links.github}</button>
-        </div>
+        {data.links && (
+          <div className="link--container">
+            <button
+              className="link"
+              onClick={() => (window.location.href = data.links.live)}
+            >
+              View
+            </button>
+            <button
+              className="link"
+              onClick={() => (window.location.href = data.links.github)}
+            >
+              Code
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,47 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import ProjectCard from "./ProjectCard";
 import data from "../data/data.json";
+import back from "../assets/back.svg";
 
 export default function Projects() {
+  let navigate = useNavigate();
   //map the data contents into individual cards
   let projectMap = data.map((item) => {
     return <ProjectCard key={item.id} data={item} />;
   });
-  return <div className="project--container">{projectMap}</div>;
+
+  //manage state for filter
+  let [filter, setFilter] = useState("All");
+
+  //create array for filter
+  let filterArr = ["All", "Full stack", "Frontend only", "Backend only"];
+
+  //map the array
+  let filterMap = filterArr.map((item) => {
+    return (
+      <p
+        key={item}
+        className={`filter ${filter === item ? "active" : ""}`}
+        onClick={() => setFilter(item)}
+      >
+        {item}
+      </p>
+    );
+  });
+
+  return (
+    <div className="project--container">
+      <div className="filter--container">
+        <img
+          className="back"
+          src={back}
+          alt="back"
+          onClick={() => navigate(-1)}
+        ></img>
+        {filterMap}
+      </div>
+      <div className="card--container">{projectMap}</div>
+    </div>
+  );
 }
