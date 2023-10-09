574bc111145dea38f187167d96c305823b8b4e2b
Girish_N <ngirish1729@gmail.com>
2023-10-09 15:07:04 +0530

    redesign

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,13 @@
 export default function About() {
-  return <div className="about--container">About</div>;
+  return (
+    <div className="about--container">
+      Hello there! My name is Girish. I&#39;m a web developer with a passion for
+      front-end technologies.
+      <br />I specialize in building responsive websites and web applications
+      using HTML, CSS, and JavaScript. I have experience working with modern
+      front-end frameworks such as React. I always look for new challenges,
+      opportunities to learn and grow as a developer. I love to travel and
+      explore new places.
+    </div>
+  );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,51 @@
+import api from "../data/contactAPI.json";
+
 export default function Contact() {
-  return <div className="contact--container">Contact</div>;
+  return (
+    // basic inputs and form with POST method calling the personal conatctAPI
+
+    <form action={`${api[0].url}`} id="contact--form" method="POST">
+      <div className="contact--container">
+        <p className="contact--head">Contact me</p>
+        <input
+          type="text"
+          className="contact--input"
+          name="contactName"
+          placeholder="Name"
+          autoFocus
+          required
+          autoComplete="false"
+        ></input>
+        <input
+          type="email"
+          className="contact--input"
+          name="contactMail"
+          placeholder="e-mail"
+          required
+          autoComplete="false"
+          autoCapitalize="false"
+        ></input>
+        <input
+          type="text"
+          className="contact--input"
+          name="contactSubject"
+          placeholder="Message Subject"
+          required
+          autoComplete="false"
+        ></input>
+        <textarea
+          rows={5}
+          type="text"
+          className="contact--input"
+          name="contactMessage"
+          placeholder="Message"
+          required
+          autoComplete="false"
+        ></textarea>
+        <button type="submit" className="contact--send">
+          Send
+        </button>
+      </div>
+    </form>
+  );
 }
