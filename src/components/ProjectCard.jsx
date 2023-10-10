4556795575f2e099ac1027cefcca3152d3af47bb
Girish_N <ngirish1729@gmail.com>
2023-10-10 15:01:51 +0530

    deployment updates, svg

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,6 +3,8 @@ import summarizer from "../assets/summarizer_live.png";
 import chatbot from "../assets/chatbot_live.png";
 import portfolio from "../assets/portfolio_live.png";
 import eCommerce from "../assets/e-commerce_live.png";
+import link from "../assets/link.svg";
+import live from "../assets/live.svg";
 
 export default function ProjectCard({ data }) {
   let imgSource =
@@ -16,26 +18,37 @@ export default function ProjectCard({ data }) {
   return (
     <div className="project--card">
       <div className="project--head">
-        <p className="project--tech">{data.tech}</p>
+        <p className="project--tech">[Tech: {data.tech}]</p>
         <p className="project--title">{data.name}</p>
       </div>
-      <img className="project--img" src={imgSource} alt="project image"></img>
+      {data.links && (
+        <img className="project--img" src={imgSource} alt="project image"></img>
+      )}
       <div className="project--body">
         <p className="project--desc">{data.desc}</p>
-        <div className="links--container">
-          <button
-            className="live link"
-            onClick={() => (window.location.href = `${data.links.live}`)}
-          >
-            View
-          </button>
-          <button
-            className="github link"
-            onClick={() => (window.location.href = `${data.links.github}`)}
-          >
-            Code
-          </button>
-        </div>
+        {data.api && (
+          <p className="project--desc">
+            <strong>Backend service:</strong> {data.api}
+          </p>
+        )}
+        {data.links && (
+          <div className="links--container">
+            <button
+              className="live link"
+              onClick={() => (window.location.href = `${data.links.live}`)}
+            >
+              <img className="download" src={live} alt="live link"></img>
+              View website
+            </button>
+            <button
+              className="github link"
+              onClick={() => (window.location.href = `${data.links.github}`)}
+            >
+              <img className="download" src={link} alt="repository link"></img>
+              View repository on github
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
