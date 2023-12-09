9436c7ffa4c5ed3dfedfe752ecaad063661f090b
Girish_N <ngirish1729@gmail.com>
2023-12-09 21:29:04 +0530

    added error route

---
+++ src/components/Error.jsx
@@ -0,0 +1,3 @@
+export default function Error() {
+  return <div className="error--container">404 error. Page not found</div>;
+}
--- src/components/SwitchPage.jsx
+++ src/components/SwitchPage.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Misc from "./Misc";
+import Error from "./Error";
 
 export default function SwitchPage() {
   return (
     <Routes key={location.pathname}>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/misc" element={<Misc />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
