c1934086d8580547695727af46c23f26276247f6
Girish_N <ngirish1729@gmail.com>
2023-12-13 21:26:08 +0530

    error page update

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,3 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import errorIcon from "../assets/404.svg";
+
 export default function Error() {
-  return <div className="error--container">404 error. Page not found</div>;
+  let navigate = useNavigate();
+  return (
+    <div className="error--container">
+      <div className="error--prompt">
+        <img src={errorIcon}></img>
+        <p>404. Page not found</p>
+      </div>
+      <p>Are you sure you have entered the correct URL?</p>
+      <button className="error--btn" onClick={() => navigate("/")}>
+        Back to Home
+      </button>
+    </div>
+  );
 }
